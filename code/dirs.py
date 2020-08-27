7aeaf90e1c89c05dfa1e23ceb0f7260b459e9852
bradkav <bradley.kavanagh@cantab.net>
2020-08-27 10:56:18 +0200

    Changed how directory definitions work, see dirs.py

---
+++ code/dirs.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+#montecarlo_dir = "AMC_montecarlo_data/"
+#data_dir = "/Users/bradkav/Dropbox/Projects/Axion star radio detection/current/data_ecc/"
+montecarlo_dir = "AMC_montecarlo_data/"
+data_dir = "/Users/bradkav/Dropbox/Projects/Axion star radio detection/current/data_ecc/"
