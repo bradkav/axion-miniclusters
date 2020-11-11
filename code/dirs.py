43b9508bd320539904d5dab708bb34d8d92daf2f
Bradley J. Kavanagh <bradley.kavanagh@cantab.net>
2020-11-12 00:03:00 +0100

    Update dirs.py

--- code/dirs.py
+++ code/dirs.py
@@ -2,6 +2,7 @@
 import os
 
 #Specify the locations of the Monte Carlo simulations and data directories here.
+#Edit these variables to point to the right place for you.
 
 data_dir = "/Users/bradkav/Projects/AMC_encounters/axion-miniclusters/data/"
 montecarlo_dir = "/Users/bradkav/Projects/AMC_encounters/code/AMC_montecarlo_data/"
