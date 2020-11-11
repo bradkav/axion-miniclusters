72663cbfb94d6525baa17f642e726e9cb6f7da50
bradkav <bradley.kavanagh@cantab.net>
2020-11-11 23:49:46 +0100

    Moving files around

--- code/GetSignals.sh
+++
@@ -1,8 +0,0 @@
-ASflag="-AScut"
-
-Nsig=1e4
-
-python simulate_signal.py -profile PL -unperturbed 0 $ASflag -Ne $Nsig
-python simulate_signal.py -profile PL -unperturbed 1 $ASflag -Ne $Nsig
-python simulate_signal.py -profile NFW -unperturbed 0 $ASflag -Ne $Nsig
-python simulate_signal.py -profile NFW -unperturbed 1 $ASflag -Ne $Nsig
