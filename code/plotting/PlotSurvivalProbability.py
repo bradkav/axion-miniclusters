0ed27e5cc56366c677db5a34a809b06046ad649f
bradkav <bradkav@gmail.com>
2021-07-15 16:04:39 +0200

    Updated survival probability

--- code/plotting/PlotSurvivalProbability.py
+++ code/plotting/PlotSurvivalProbability.py
@@ -13,7 +13,7 @@ frac_AScut_PL = 0.0002719
 
 #IDstr = "_gamma-0.5"
 #IDstr = ""
-IDstr = "_wStripping"
+IDstr = "_ma_400mueV"
 
 #To-Do - make the correction for the initial range of samples masses more concrete
 
